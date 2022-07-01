8494d44b02d9d3dd8b746b3b561f200bde949dc2
Barbito <thedarkness355@gmail.com>
2022-07-01 09:26:36 +0200

    Cube movement by Swap

--- Assets/Move.cs
+++ Assets/Move.cs
@@ -7,6 +7,7 @@ public class Move : MonoBehaviour
     public Vector3 midPosition;
     public Vector3 rightPosition;
     public Vector3 leftPosition;
+    public int movement = 3;
 
     // Start is called before the first frame update
     void Start()
@@ -18,46 +19,41 @@ public class Move : MonoBehaviour
     // Update is called once per frame
     void Update()
     {
-        if (Input.GetKey(KeyCode.RightArrow) || Input.GetKey(KeyCode.D)) 
+        if (Input.GetKeyDown(KeyCode.RightArrow) || Input.GetKeyDown(KeyCode.D)) 
         {
             if (Vector3.Distance(transform.position, midPosition) < 0.1)
             {
-                transform.position = rightPosition;
+                float getIndented = transform.position.x + movement;
+                Vector3 newRightPosition = new Vector3(getIndented, transform.position.y, transform.position.z);
+                transform.position = newRightPosition;
             }
-            
-            if (Vector3.Distance(transform.position, leftPosition) < 0.1)
-            {
-                transform.position = midPosition;
+            else if (Vector3.Distance(transform.position, leftPosition) < 0.1) {
+                float getIndented = transform.position.x + movement;
+                Vector3 newMidPosition = new Vector3(getIndented, transform.position.y, transform.position.z);
+                transform.position = newMidPosition;
+            }
+            else {
+                transform.position = transform.position;
             }
         }
 
-        if (Input.GetKey(KeyCode.LeftArrow) || Input.GetKey(KeyCode.Q)) 
+        if (Input.GetKeyDown(KeyCode.LeftArrow) || Input.GetKeyDown(KeyCode.Q)) 
         {
             if (Vector3.Distance(transform.position, midPosition) < 0.1)
             {
-                transform.position = leftPosition;
+                float getIndented = transform.position.x - movement;
+                Vector3 newLeftPosition = new Vector3(getIndented, transform.position.y, transform.position.z);
+                transform.position = newLeftPosition;
             }
-            
-            if (Vector3.Distance(transform.position, rightPosition) < 0.1)
-            {
-                transform.position = midPosition;
+            else if (Vector3.Distance(transform.position, rightPosition) < 0.1) {
+                float getIndented = transform.position.x - movement;
+                Vector3 newMidPosition = new Vector3(getIndented, transform.position.y, transform.position.z);
+                transform.position = newMidPosition;                
             }
-            
-            // if (transform.position == leftPosition)
-            // {
-            //     transform.position = midPosition;
-            // }
+            else {
+                transform.position = transform.position;
+            }
         }
-        
-        // if (Input.GetKey(KeyCode.RightArrow)) 
-        // {
-        //     transform.position = rightPosition
-        // }
-
-        // if (Input.GetKey(KeyCode.LeftArrow)) 
-        // {
-        //     transform.position = leftPosition
-        // }
     }
     
 }
