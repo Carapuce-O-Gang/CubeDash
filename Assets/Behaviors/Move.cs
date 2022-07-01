1690452d679bccb3097ce4d22ceee72ffbccad06
Tracks12 <cardinal.florian.erwan@gmail.com>
2022-07-01 10:14:46 +0200

    fix move behaviors

---
+++ Assets/Behaviors/Move.cs
@@ -0,0 +1,43 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Move : MonoBehaviour
+{
+    public int axis;
+
+    private float movement = 1.0f;
+    private float Xmax;
+    private float Xmin;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        this.Xmax = this.transform.position.x + this.axis-1;
+        this.Xmin = this.transform.position.x;
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+        if ((Input.GetKeyDown(KeyCode.RightArrow) || Input.GetKeyDown(KeyCode.D)) && this.transform.position.x > this.Xmin) 
+        {
+            this.transform.position = new Vector3(
+                this.transform.position.x - this.movement,
+                this.transform.position.y,
+                this.transform.position.z
+            );
+        }
+
+        if ((Input.GetKeyDown(KeyCode.LeftArrow) || Input.GetKeyDown(KeyCode.Q)) && this.transform.position.x < this.Xmax) 
+        {
+            this.transform.position = new Vector3(
+                this.transform.position.x + this.movement,
+                this.transform.position.y,
+                this.transform.position.z
+            );
+        }
+    }
+    
+}
