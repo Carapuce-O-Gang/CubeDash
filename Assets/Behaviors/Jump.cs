b9511a6706d96e7529e8cd6071af8dd609b3d5c1
Decloquement Oscar <odecloquement@gmail.com>
2022-07-01 10:24:15 +0200

    [CUBE-0003] Update jump and speed

--- Assets/Behaviors/Jump.cs
+++ Assets/Behaviors/Jump.cs
@@ -1,3 +1,4 @@
+using System;
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
@@ -5,8 +6,10 @@ using UnityEngine;
 public class Jump : MonoBehaviour
 {
     public float speed = 0.1f;
-    public float jumpSpeed = 8f;
+    public float jumpSpeed = 6f;
     public float distToGround = 0.5f;
+    public bool x, z;
+    public bool reverseSpeed = false;
     Rigidbody rb;
     // Start is called before the first frame update
     void Start()
@@ -17,17 +20,25 @@ public class Jump : MonoBehaviour
     // Update is called once per frame
     void FixedUpdate()
     {
+        if (reverseSpeed)
+        {
+            speed = speed * -1;
+        }
         if (Input.GetKey(KeyCode.Space) && isGrounded())
         {
-            Vector3 jumpVelocity = new Vector3(0f, jumpSpeed, 0f);
-            rb.velocity = rb.velocity + jumpVelocity;
+            rb.AddForce(new Vector3(0,jumpSpeed), ForceMode.Impulse);
         }
-
-        float horizontal = Input.GetAxis("Horizontal");
-        float vertical = Input.GetAxis("Vertical");
-        Vector3 movement = new Vector3(horizontal * speed * Time.deltaTime, 0, vertical * speed * Time.deltaTime);
-        rb.MovePosition(transform.position + movement);
-        transform.Translate(speed, 0f, 0f);
+        
+        if (x)
+        {
+            Vector3 movementX = new Vector3(speed, 0, 0);
+            rb.MovePosition(transform.position + movementX);
+        }
+        else if (z)
+        {
+            Vector3 movementZ = new Vector3(0, 0, speed);
+            rb.MovePosition(transform.position + movementZ);
+        }
     }
     bool isGrounded()
     {
